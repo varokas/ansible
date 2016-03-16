100ea6ee257b0d7ec6418aa038e65b73edd2eb8e
Varokas Panusuwan <varokas@apple.com>
2016-03-15 19:44:38 -0700

    add initial scripts

---
+++ scripts/init_user.sh
@@ -0,0 +1,1 @@
+pub=`cat ~/.ssh/varokas.pub`; ssh -i ~/.ssh/varokas root@$1 "useradd varokas; gpasswd -a varokas sudo; mkdir -p /home/varokas/.ssh; echo $pub > /home/varokas/.ssh/authorized_keys; chown -R varokas /home/varokas/.ssh"
